a953c8203073a398bf6106b2a50fa3f6389dde1d
siraajul <shirajulislamparvez@gmail.com>
2024-04-16 03:48:05 +0600

    Remove Unused Imports

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
 
