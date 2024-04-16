5fbb96db48757eec9e2779c7887b6e647f0875a0
siraajul <shirajulislamparvez@gmail.com>
2024-04-16 16:36:30 +0600

    enable horizontal scroll view

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -1,24 +1,26 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 export default function FlatCards() {
   return (
     <View>
       <Text style={styles.headingText}>Flat Cards</Text>
-      <View style={styles.container}>
-        <View style={[styles.card, styles.cardOne]}>
-          <Text>Red</Text>
+      <ScrollView horizontal={true}>
+        <View style={styles.container}>
+          <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+          </View>
+          <View style={[styles.card, styles.cardTwo]}>
+            <Text>Green</Text>
+          </View>
+          <View style={[styles.card, styles.cardThree]}>
+            <Text>Blue</Text>
+          </View>
+          <View style={[styles.card, styles.cardFour]}>
+            <Text>Yellow</Text>
+          </View>
         </View>
-        <View style={[styles.card, styles.cardTwo]}>
-          <Text>Green</Text>
-        </View>
-        <View style={[styles.card, styles.cardThree]}>
-          <Text>Blue</Text>
-        </View>
-        <View style={[styles.card, styles.cardFour]}>
-          <Text>Yellow</Text>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
